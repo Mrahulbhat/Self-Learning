a6eab4d9619dccc4af1a49f31b4940596a88ca99
Mrahulbhat <rahulbhat5122@gmail.com>
2025-03-08 13:18:51 +0530

    updated

--- Angular/First-app/first-app/README.md
+++ Angular/First-app/first-app/README.md
@@ -1,6 +1,6 @@
-# AngProject
+# FirstApp
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.5.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.1.
 
 ## Development server
 
--- Angular/First-app/first-app/src/app/app.component.spec.ts
+++ Angular/First-app/first-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ang-project' title`, () => {
+  it(`should have the 'first-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-project');
+    expect(app.title).toEqual('first-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ang-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first-app');
   });
 });
