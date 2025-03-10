2e373098adf68796b284984553d15c0428ff0268
M Rahul Bhat <mrahulbhat@Rahuls-Mac-M4.local>
2025-03-10 23:42:39 +0530

    updated

--- Angular/Demo-project/first-app/src/app/app.component.ts
+++ Angular/Demo-project/first-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from '../rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RoomsComponent],
+  imports: [RoomsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Angular/Demo-project/first-app/src/rooms/rooms.component.ts
+++ Angular/Demo-project/first-app/src/rooms/rooms.component.ts
@@ -1,31 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export interface Room{
+  totalRooms:number;
+  availableRooms:number;
+  bookedRooms:number;
+}
+
+export interface RoomList{
+  roomtype:string;
+  amenities:string;
+  price:number;
+  checkintime:Date;
+  checkouttime:Date;
+}
+
 @Component({
   selector: 'app-rooms',
   imports: [CommonModule],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.css'
 })
-export class RoomsComponent {
-  Hotelname = "Ocean Pearl"
-  rooms = [
-    {
-      category: "Standard",
-      price: 1000
-    },
-    {
-      category: "Semi-Luxury",
-      price: 2000
-    },
-    {
-      category: "Full-Luxury",
-      price: 3000
-    }
-  ];
+export class RoomsComponent{
 
+  Hotelname = "Ocean Pearl";
   hideRooms = false;
 
-  toggle(){
-    this.hideRooms=!this.hideRooms;
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
   }
-}
+
+  roomList:RoomList[] = [
+    {
+      roomtype:'Standard Room',
+      amenities:"basic",
+      price:100,
+      checkintime:new Date(2025,10,1,10,30,0,0),
+      checkouttime:new Date(2025,10,2,10,30,0,0)
+
+    },
+    {
+      roomtype:'Semi-Deluxe Room',
+      amenities:"soft-bed, AC",
+      price:100,
+      checkintime:new Date(2025,9,1,10,30,0,0),
+      checkouttime:new Date(2025,9,2,10,30,0,0)
+
+    },
+    {
+      roomtype:'Deluxe Room',
+      amenities:"Ultra soft bed, AC, Bath Tub ",
+      price:100,
+      checkintime:new Date(2025,8,1,8,30,0,0),
+      checkouttime:new Date(2025,8,2,10,30,0,0)
+    }
+  ];
+};
